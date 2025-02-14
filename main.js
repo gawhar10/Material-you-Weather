23f03dfc69eac17de6da2a1b85c5ae8c2b75763c
Gawhar Salman <gawharsalman123@gmail.com>
2025-02-14 17:49:40 +0530

    deleted:    forecast.json 	modified:   index.html 	modified:   main.js 	new file:   modules.js

--- main.js
+++ main.js
@@ -1,109 +1,4 @@
-/* Get cities from geocoding-api.open-meteo.com. */
-const getCities = async function (cityName) {
-  try {
-    url = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`;
-    let cities = await fetch(url);
-    return cities.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/* Get weather based on city latitude and longitude. */
-const getWeather = async function (latitude, longitude) {
-  try {
-    url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum&timezone=auto`;
-    let weatherData = await fetch(url);
-    return weatherData.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/* Format ISO date to more readable form. */
-const formatISODate = (isoDate) => {
-  const date = new Date(isoDate);
-  const time = `${date.getHours()}:${date.getMinutes()}`;
-  const day = `${date.getDate()}/${date.getMonth() + 1}`;
-  console.log({ time: time, day: day });
-  return { time: time, day: day };
-};
-
-/* Format common date. */
-const formatDate = (date) => {
-  //   date = "2024-09-01";
-  date = date.split("-");
-  const [year, month, day] = date;
-  const monthNames = [
-    "jan",
-    "feb",
-    "mar",
-    "apr",
-    "may",
-    "jun",
-    "jul",
-    "aug",
-    "sept",
-    "oct",
-    "nov",
-    "dec",
-  ];
-  return `${monthNames[Number(month) - 1]}, ${day}`;
-};
-
-/* Calculate day time in hours and minutes. */
-const dayDuration = (sunrise, sunset) => {
-  sunrise = sunrise.split(":");
-  console.log(sunrise);
-  sunset = sunset.split(":");
-  console.log(sunset);
-  const hour = Number(sunset[0]) - Number(sunrise[0]);
-  const minutes = Number(sunset[1]) - Number(sunrise[1]);
-  if (Number(sunset[1]) > Number(sunrise[1])) {
-    return { hour: `${hour}`, minutes: `${minutes}` };
-  } else {
-    return { hour: `${hour - 1}`, minutes: `${60 + minutes}` };
-  }
-};
-
-/* Update weather code */
-const setWeatherString = (weatherCode) => {
-  const weatherCodes = {
-    "Clear sky": 0,
-    "Mainly clear": 1,
-    "Partly cloudy": 2,
-    Overcast: 3,
-    Fog: 45,
-    "Depositing fime fog": 48,
-    "Light drizzle": 51,
-    "Moderate drizzle": 53,
-    "Dense drizzle": 55,
-    "Light freezing drizzle": 56,
-    "Moderate or dense freezing drizzle": 57,
-    "Light rain": 61,
-    "Moderate rain": 63,
-    "Heavy rain": 65,
-    "Light freezing rain": 66,
-    "Moderate or heavy freezing rain": 67,
-    "Slight snowfall": 71,
-    "Moderate snowfall": 73,
-    "Heavy snowfall": 75,
-    "Snow grains": 77,
-    "Slight rain showers": 80,
-    "Moderate rain showers": 81,
-    "Heavy rain showers": 82,
-    "Slight snow showers": 85,
-    "Heavy snow showers": 86,
-    "Thunderstorm slight or moderate": 95,
-    "Thunderstorm strong": 96,
-    "Thunderstorm heavy": 99,
-  };
-  Object.entries(weatherCodes).forEach(([key, val]) => {
-    if (val === weatherCode) {
-      document.querySelector("#weatherType").innerHTML = `${key}`;
-    }
-  });
-};
+import { getCities, getWeather, formatISODate, formatDate, dayDuration, setWeatherString } from "./modules.js";
 
 /* Update city name on UI. */
 const updateCity = (city) => {
@@ -210,6 +105,7 @@ const updateDailyWeather = (days, minTemp, maxTemp, rainSum) => {
 const updateWeather = function (cities) {
   const generatedCityList = document.querySelector("#generatedCityList");
   const ul = document.createElement("ul");
+
   cities = cities.results;
 
   cities.forEach((city) => {
@@ -241,8 +137,12 @@ const updateWeather = function (cities) {
       }
     });
     ul.appendChild(li);
-    generatedCityList.appendChild(ul);
   });
+  // remove previous ul.
+  if (generatedCityList.hasChildNodes()) {
+    generatedCityList.innerHTML = "";
+  }
+  generatedCityList.appendChild(ul);
 };
 
 const refreshWeather = async (savedCity) => {
@@ -259,8 +159,10 @@ const refreshWeather = async (savedCity) => {
 document.querySelector("#searchBtn").addEventListener("click", async (e) => {
   e.preventDefault();
   const cityInput = document.querySelector("#cityInput");
-  const cities = await getCities(cityInput.value);
-  updateWeather(cities);
+  if (cityInput.value.match(/[A-Za-z]+/)) {
+    const cities = await getCities(cityInput.value);
+    updateWeather(cities);
+  }
 });
 
 /* Refresh functionality event. */
